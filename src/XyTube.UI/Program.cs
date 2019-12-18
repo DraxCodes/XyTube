a9e91359f78975633accab01acd3b6805eb934b9
Joel Parkinson <joelparkinson@live.co.uk>
2019-12-18 18:11:08 +0000

    Initial Project & Service collection setup

---
+++ src/XyTube.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XyTube.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var setup = Activator.CreateInstance<Setup>();
+        }
+    }
+}
---
+++ src/XyTube.UI/Setup.cs
@@ -0,0 +1,26 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XyTube.UI
+{
+    internal sealed class Setup
+    {
+        public readonly IKernel Kernel;
+
+        internal Setup()
+        {
+            Kernel = new StandardKernel(new XyModule());
+        }
+    }
+
+    internal sealed class XyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
